a8b80fb9948f14b8779f8e2bf36ce7b7dcd4d3e7
francisco guedes <kikoguedes.33@gmail.com>
2019-02-23 00:46:05 +0000

    deploy 1.1
    Started to change visuals on both index and effect pages.

--- api/api.py
+++ api/api.py
@@ -1,32 +1,37 @@
-from flask import Flask, request, send_file
-import time
 import glob
 import os
+import time
+
+from flask import Flask, request, send_file
+
 app = Flask(__name__)
 
-
-ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = ['pdf', 'png', 'jpg', 'jpeg']
 
 
 @app.route('/effect<int:number>/uploader', methods=['GET', 'POST'])
 def upload_file(number):
-   if request.method == 'POST':
-      f = request.files['file']
-      if f == None:
-          return 'no upload'
-      if f.filename.split(".")[-1] not in ALLOWED_EXTENSIONS:
-          return 'file not uploaded - extension error'
-      else:
-          id = str(time.time()).split('.')[0]
-          filename ='../imagedb/effect' + str(number) + '_' + id + '.' + f.filename.split(".")[-1]
-          f.save(filename)
-          os.system('python3 ../effects/japanify.py ' + filename)
-          return 'file uploaded with ' + id
+    if request.method == 'POST':
+        f = request.files['file']
+        if f is None:
+            return 'no upload'
+        if f.filename.split(".")[-1] not in ALLOWED_EXTENSIONS:
+            return 'file not uploaded - extension error'
+        else:
+            id = str(time.time()).split('.')[0]
+            filename = '../imagedb/effect' + str(number) + '_' + id + '.' + f.filename.split(".")[-1]
+            f.save(filename)
+            os.system('python3 ../effects/japanify.py ' + filename)
+            os.remove(
+                os.path.join('..', 'imagedb', 'effect' + str(number) + '_' + id + '.' + f.filename.split(".")[-1]))
+            link = '<a href=' + '"http://193.136.167.233:5000/images/download/' + id + '">' + 'Download Image' + '</a>' + '<br>'
+
+            return 'file uploaded with ' + id + '\n' + link
 
 
 @app.route('/effect<int:number>/images', methods=['GET'])
 def get_images(number):
-    return (str(glob.glob('../effects_applied/effect' + str(number) + '*')))
+    return str(glob.glob('../effects_applied/effect' + str(number) + '*'))
 
 
 @app.route('/images', methods=['GET'])
@@ -39,13 +44,24 @@ def show_all_downloadable_images():
     result = ""
     for dir in glob.glob('../effects_applied/*'):
         file_id = dir.split("_")[-1].split(".")[0]
-        result += ('<a href=' + '"http://194.210.220.222:5000/images/download/' + file_id + '">' + file_id +'</a>' + '<br>')
+        result += (
+                    '<a href=' + '"http://193.136.167.233:5000/images/download/' + file_id + '">' + file_id + '</a>' + '<br>')
     return result
+
 
 @app.route('/images/download/<int:id>', methods=['GET'])
 def download_image(id):
-    return send_file(str(glob.glob('../effects_applied/*'+ str(id) + '*')[0]))
+    return send_file(str(glob.glob('../effects_applied/*' + str(id) + '*')[0]))
 
+
+@app.route('/images/download/effect<int:number>', methods=['GET'])
+def show_all_downloadable_images_by_effect(number):
+    result = ""
+    for dir in glob.glob('../effects_applied/effect' + str(number) + '*'):
+        file_id = dir.split("_")[-1].split(".")[0]
+        result += (
+                    '<a href=' + '"http://193.136.167.233:5000/images/download/' + file_id + '">' + file_id + '</a>' + '<br>')
+    return result
 
 
 if __name__ == "__main__":
